e40d274c89e15693557f1e4dbc8bd8d2d9610437
Jace Goetsch <goetsch.jace@gmail.com>
2020-11-18 13:09:41 -0700

    Finished Plots and saved them as PNGs

--- Electric Power Consum Plots.R
+++ Electric Power Consum Plots.R
@@ -14,9 +14,56 @@ head(EPC)
 EPC$Global_active_power <- as.numeric(EPC$Global_active_power)
 EPC$Global_reactive_power <- as.numeric(EPC$Global_reactive_power)
 voltage <- as.numeric(EPC$Voltage)
-subMetering1 <- as.numeric(EPC$Sub_metering_1)
-subMetering2 <- as.numeric(EPC$Sub_metering_2)
-subMetering3 <- as.numeric(EPC$Sub_metering_3)
+EPC$Sub_metering_1 <- as.numeric(EPC$Sub_metering_1)
+EPC$Sub_metering_2 <- as.numeric(EPC$Sub_metering_2)
+EPC$Sub_metering_3 <- as.numeric(EPC$Sub_metering_3)
 
-#Plot one Histogram 
+#Plot 1 
 hist(EPC$Global_active_power,col="red",main="Global Active Power",xlab = "Global Active Power (kilowatts)")
+#Saving file
+dev.copy(png,"plot1.png", width=480, height=480)
+dev.off()
+
+#Plot 2
+#create DateTime column
+EPC$DateTime <-paste(EPC$Date,EPC$Time)
+EPC$DateTime <- as.POSIXct(EPC$DateTime)
+plot(EPC$Global_active_power~EPC$DateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+#Saving file
+dev.copy(png,"plot2.png", width=480, height=480)
+dev.off()
+
+#Plot 3 
+
+with(EPC, {
+  plot(Sub_metering_1~DateTime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~DateTime,col='Red')
+  lines(Sub_metering_3~DateTime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+#Saving file
+dev.copy(png, file="plot3.png", height=480, width=480)
+dev.off()
+
+#4  
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(EPC, {
+  plot(Global_active_power~DateTime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Voltage~DateTime, type="l", 
+       ylab="Voltage (volt)", xlab="")
+  plot(Sub_metering_1~DateTime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~DateTime,col='Red')
+  lines(Sub_metering_3~DateTime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~DateTime, type="l", 
+       ylab="Global Rective Power (kilowatts)",xlab="")
+})
+
+#Saving file
+dev.copy(png, file="plot4.png", height=480, width=480)
+dev.off()
